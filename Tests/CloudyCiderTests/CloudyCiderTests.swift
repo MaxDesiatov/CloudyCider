9755b22741f3445808172b73499c2929cfdca09c
Max Desiatov <max@desiatov.com>
2019-06-24 12:10:57 +0100

    Initial commit

---
+++ Tests/CloudyCiderTests/CloudyCiderTests.swift
@@ -0,0 +1,23 @@
+//
+//  CloudyCiderTests.swift
+//  CloudyCider
+//
+//  Created by Max Desiatov on 04/05/2019.
+//  Copyright © 2019 CloudyCider. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import CloudyCider
+
+class CloudyCiderTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(CloudyCider().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import CloudyCiderTests
+
+XCTMain([
+    testCase(CloudyCiderTests.allTests),
+])
